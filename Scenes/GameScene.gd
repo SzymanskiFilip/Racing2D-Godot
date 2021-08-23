53635d07b47998dd77e0ec6f0083ffa3d011016e
Filip <szymanski.fp@gmail.com>
2021-08-23 20:16:29 +0200

    preloaded obstacle and spawning it every second in the GameScene

--- Scenes/GameScene.gd
+++ Scenes/GameScene.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
+const OBSTACLE = preload("res://Scenes/Obstacle.tscn")
 
-func _ready():
-	pass
+
+func _on_Timer_timeout():
+	print("SPAWN")
+	var obstacle = OBSTACLE.instance()
+	add_child(obstacle)
 	
