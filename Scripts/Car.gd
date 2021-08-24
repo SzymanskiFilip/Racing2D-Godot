ec6d85ce967083126780d3d2c8c106146fd8c089
Filip <szymanski.fp@gmail.com>
2021-08-24 21:56:59 +0200

    emiting signal on car collision

--- Scripts/Car.gd
+++ Scripts/Car.gd
@@ -7,4 +7,12 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
 		if position.x > 15:
 			position.x -= 7
+			
 
+signal LOST
+
+
+func _on_Area2D_area_entered(area):
+	print("COLLIDED AREA")
+	emit_signal("LOST")
+	
