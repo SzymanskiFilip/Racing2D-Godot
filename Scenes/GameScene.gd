7546aa56f9cedf7cab2da2c31ad13348ecbb6e0d
Filip <szymanski.fp@gmail.com>
2021-08-24 21:55:52 +0200

    change scene on car collision

--- Scenes/GameScene.gd
+++ Scenes/GameScene.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
 const OBSTACLE = preload("res://Scenes/Obstacle.tscn")
+var randomizer = RandomNumberGenerator.new()
 
 
 func _on_Timer_timeout():
-	print("SPAWN")
 	var obstacle = OBSTACLE.instance()
+	var randomXPosition = randomizer.randi_range(10, 500)
+	obstacle.position.x = randomXPosition
 	add_child(obstacle)
 	
+
+func change_scene():
+	get_tree().change_scene("res://Scenes/EndScreen.tscn")
+
+func _on_Car_LOST():
+	print("CHANGE TO LOSE SCENE")
+	change_scene()
